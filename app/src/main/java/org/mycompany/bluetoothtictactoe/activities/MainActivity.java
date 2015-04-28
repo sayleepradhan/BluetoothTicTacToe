21e09512d5b9072a448b75e3336f7a1b4a1ff9fc
malikapahva <malika.pahva@gmail.com>
2015-04-28 09:17:23 -0500

    Minor Changes

--- app/src/main/java/org/mycompany/bluetoothtictactoe/activities/MainActivity.java
+++ app/src/main/java/org/mycompany/bluetoothtictactoe/activities/MainActivity.java
@@ -41,31 +41,6 @@ public class MainActivity extends SampleActivityBase {
         }
     }
 
-    /**
-     * This method sets the menu on action bar.
-     *
-     * @param
-     *
-     * @return boolean
-     */
-
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }*/
-
-   /* @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
 
     /**
      * This method set up the log data via fragment in TextView.
